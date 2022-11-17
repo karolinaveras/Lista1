544132374f5c851aa5e1ce3dbf2edd837fabdbe9
verask95 <115596965+verask95@users.noreply.github.com>
2022-11-17 20:06:14 -0300

    Update Q7.py

--- Q7.py
+++ Q7.py
@@ -2,3 +2,7 @@ altura = float(input('Digite sua altura:'))
 
 peso = float(input('Digite o seu peso:'))
 
+imc = altura / peso **2
+
+if imc == 18.5:
+    print(f'Seu IMC é {imc}, você esta abaixo do peso')
