425ed83a371c25322c90b93edfcc766e63789a27
verask95 <115596965+verask95@users.noreply.github.com>
2022-11-17 19:56:39 -0300

    Update Q2.py

---
+++ Q2.py
@@ -0,0 +1,11 @@
+AX = int(input('digite o primeiro ponto:'))
+
+AY = int(input('digite o segundo ponto:'))
+
+BX = int(input('digite o terceiro ponto:'))
+
+BY = int(input('digite o quarto ponto:'))
+
+PLANO = ((BX - AX) **2 + (BY - AY) **(1/2))
+
+print(f"a distancia entre os dois pontos é{PLANO}")
