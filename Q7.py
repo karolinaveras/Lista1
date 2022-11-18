acf17ef7573d37d0a364cc5ceb6f3d710c1b604b
Karolina Veras <mariakarolina_veras@hotmail.com>
2022-11-17 23:18:23 -0300

    Update Q7.py

--- Q7.py
+++ Q7.py
@@ -7,16 +7,16 @@ imc = peso / altura **2
 if imc <= 18.5:
     print(f'Seu IMC é {imc}, você esta abaixo do peso!')
 
-elif imc == 18.5 or 24.9:
+elif imc == 18.5 and 24.9:
     print(f'Seu IMC é {imc}, você esta com o peso normal!')
 
-elif imc == 25 or 29.9:
+elif imc == 25 and 29.9:
     print(f'Seu IMC é {imc}, você esta com sobrepeso!')
 
-elif imc == 30 or 34.9:
+elif imc == 30 and 34.9:
     print(f'Seu IMC é {imc} e indica obesidade Grau I')
 
-elif imc == 35 or 39.9:
+elif imc == 35 and 39.9:
     print(f'Seu IMC é {imc}, você está com obesidade de grau II')
 
 elif imc >= 40:
